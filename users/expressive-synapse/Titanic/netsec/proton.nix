31ff93e55b8ebb5449fff3877fca9baa06651860
Expressive-Synapse <ConnorDGoff@protonmail.com>
2024-05-03 19:19:43 -0400

    persist protonmail logs

--- users/expressive-synapse/Titanic/netsec/proton.nix
+++ users/expressive-synapse/Titanic/netsec/proton.nix
@@ -5,4 +5,9 @@ home.packages = with pkgs; [
   protonvpn-gui
   protonmail-bridge-gui
 ];
+  home.persistence."/persist/home/expressive-synapse" = {
+  directories = [
+    ".local/share/protonmail"
+  ];
+};
 }

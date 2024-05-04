013a5a9a262dd2df48802d8c440f00ad5a0e31ff
Expressive-Synapse <ConnorDGoff@protonmail.com>
2024-05-04 12:28:28 -0400

    adjusted sambamount.nix for new secrets location

--- hosts/Titanic/netsec/sambamount.nix
+++ hosts/Titanic/netsec/sambamount.nix
@@ -14,7 +14,7 @@ fileSystems."/home/expressive-synapse/Servers/ServerBoy/jellyfiles" = {
   options = let
     # this line prevents hanging on network split
     automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"; 
-  in ["${automount_opts},credentials=/etc/nixos/secrets/samba-secrets,uid=1000,gid=100"];
+  in ["${automount_opts},credentials=/persist/NixOSDots/secrets/samba-secrets,uid=1000,gid=100"];
 };
 
 fileSystems."/home/expressive-synapse/Servers/ServerBoy/microSD" = {
@@ -23,7 +23,7 @@ fileSystems."/home/expressive-synapse/Servers/ServerBoy/microSD" = {
   options = let
     # this line prevents hanging on network split
     automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"; 
-  in ["${automount_opts},credentials=/etc/nixos/secrets/samba-secrets,uid=1000,gid=100"];
+  in ["${automount_opts},credentials=/persist/NixOSDot/secrets/samba-secrets,uid=1000,gid=100"];
 
 };
 }

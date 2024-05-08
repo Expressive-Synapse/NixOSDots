e2a2b803778ae3b62f5e486ce01c1e8e9e50e78c
Expressive-Synapse <ConnorDGoff@protonmail.com>
2024-05-07 20:35:56 -0400

    added potential fix to issue with gamescope in steam

--- hosts/Titanic/gaming.nix
+++ hosts/Titanic/gaming.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }:
 
 {
+nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
+  };
+
  environment.systemPackages = with pkgs; [
   mangohud
  ];

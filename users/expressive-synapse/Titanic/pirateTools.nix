fa7ad4b64946f446ccd08509c49fc728f5584d2e
Expressive-Synapse <ConnorDGoff@protonmail.com>
2024-08-29 19:25:05 -0400

    persist local qbittorrent files

--- users/expressive-synapse/Titanic/pirateTools.nix
+++ users/expressive-synapse/Titanic/pirateTools.nix
@@ -14,6 +14,7 @@ home.persistence."/persist/home/expressive-synapse" = {
   directories = [
     ".config/casualOnTheSpot"
     ".config/qBittorrent"
+    ".local/qBittorrent"
     "Downloads/torrents"
   ];
 };

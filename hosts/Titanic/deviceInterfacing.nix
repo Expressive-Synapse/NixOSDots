eb3626d61c2bb337ba1264c89cd56c78a6296804
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-01-11 15:46:29 -0500

    added qmk keyboard support

--- hosts/Titanic/deviceInterfacing.nix
+++ hosts/Titanic/deviceInterfacing.nix
@@ -4,14 +4,21 @@
 environment.systemPackages = with pkgs; [
   mtpfs
   simple-mtpfs
-  via
   piper
   usbutils
   jmtpfs
   glib
+  via
 ];
 
 services.ratbagd.enable = true;
 
 services.gvfs.enable = true;
+
+hardware.keyboard.qmk.enable = true;
+
+services.udev.packages = with pkgs; [
+  via
+];
+
 }

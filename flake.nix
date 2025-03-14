78d65e4d98afe3939b81b08fd350fb7020c91916
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-13 20:13:44 -0400

    reorganized flake, and added nvf

--- flake.nix
+++ flake.nix
@@ -2,6 +2,73 @@
 
   description = "my Primary flake";
   
+  inputs = {
+    
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+    
+    nixpkgs-stable = {
+      url = "nixpkgs/nixos-23.11";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    xremap-flake = {
+      url = "github:xremap/nix-flake";
+    };
+
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    };
+
+    split-monitor-workspaces = {
+      url = "github:Duckonaut/split-monitor-workspaces";
+      inputs.hyprland.follows = "hyprland";
+    };
+    
+    ags = {
+      url = "github:Aylur/ags";
+    };
+
+    nvf = {
+      url = "github:notashelf/nvf";
+    };
+
+
+  };
+
   outputs = inputs@{ self, nixpkgs, nixpkgs-stable, stylix, ... }:
     let
       lib = nixpkgs.lib;
@@ -46,38 +113,5 @@
       };
     };
   };
-
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-23.11";
-
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager-stable.url = "github:nix-community/home-manager/release-23.11";
-    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
-
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-
-    impermanence.url = "github:nix-community/impermanence";
-
-    stylix.url = "github:danth/stylix";
-
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-
-    xremap-flake.url = "github:xremap/nix-flake";
-
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      inputs.hyprland.follows = "hyprland";
-    };
-    ags.url = "github:Aylur/ags";
-  };
-
+ 
 }

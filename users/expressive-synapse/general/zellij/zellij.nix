fe6b92d6341355f12a0aa1f69330bb9a6c035df1
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-14 14:26:29 -0400

    reorganized code

--- users/expressive-synapse/general/zellij/zellij.nix
+++ users/expressive-synapse/general/zellij/zellij.nix
@@ -3,6 +3,11 @@
 {
 programs.zellij.enable = true;
 
-home.file.".config/zellij/config.kdl".source = ./config.kdl;
-home.file.".config/zellij/plugins/zellij-autolock.wasm".source = ./plugins/zellij-autolock.wasm;
+home.file.".config/zellij/config.kdl" = {
+  source = ./config.kdl;
+};
+home.file.".config/zellij/plugins" = {
+  source = ./plugins;
+};
+
 }

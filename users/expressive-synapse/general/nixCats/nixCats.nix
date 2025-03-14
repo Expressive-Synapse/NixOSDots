da17c83e8bfb2708274795a7dd9ecd9e5be76b9e
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-14 17:02:18 -0400

    added vi alias

--- users/expressive-synapse/general/nixCats/nixCats.nix
+++ users/expressive-synapse/general/nixCats/nixCats.nix
@@ -156,7 +156,7 @@ in {
             wrapRc = true;
             # IMPORTANT:
             # your alias may not conflict with your other packages.
-            aliases = [ "vim" "homeVim" ];
+            aliases = [ "vi" "vim" "homeVim" ];
             # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
           };
           # and a set of categories that you want

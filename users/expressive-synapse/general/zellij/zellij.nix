845de5f01d4f8c5cd072f95538ca2fac29cd9fbf
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-14 12:59:30 -0400

    adjusted theme and plugin location

--- users/expressive-synapse/general/zellij/zellij.nix
+++ users/expressive-synapse/general/zellij/zellij.nix
@@ -4,4 +4,5 @@
 programs.zellij.enable = true;
 
 home.file.".config/zellij/config.kdl".source = ./config.kdl;
+home.file.".config/zellij/plugins/zellij-autolock.wasm".source = ./plugins/zellij-autolock.wasm;
 }

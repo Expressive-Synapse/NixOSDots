81cd1216ea2318216ff573b09f91b24380a9b1f0
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-15 20:56:53 -0400

    fixed undotree

--- users/expressive-synapse/general/nixCats/init.lua
+++ users/expressive-synapse/general/nixCats/init.lua
@@ -183,8 +183,9 @@ local lazyOptions = {
 require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", "lazy.nvim" }), {
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	"mbbill/undotree",
+  { "mbbill/undotree",
 	vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "[U]ndotree" }),
+  },
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.

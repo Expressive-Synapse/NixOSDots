02069ad888aa9dd03105f23154dc8880093e3ce1
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-19 20:18:37 -0400

    disabled listchars due to conflict indent-blankline plugin

--- users/expressive-synapse/general/nixCats/init.lua
+++ users/expressive-synapse/general/nixCats/init.lua
@@ -72,7 +72,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
@@ -681,7 +681,7 @@ require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", 
         }
       end,
       formatters_by_ft = {
-        lua = { "lua-format" },
+        lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --

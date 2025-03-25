04c8cbea9aa0898ec3656256d9a3a52801dbead2
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-25 18:41:28 -0400

    adjusted monitor number and cordinates for flameshot rule

--- hosts/Titanic/wm/hyprland.nix
+++ hosts/Titanic/wm/hyprland.nix
@@ -15,7 +15,6 @@ programs = {
     portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
 };
-wayland.windowManager.hyprland.systemd.variables = ["--all"];
 /*---------------desktop-portal------------------*/
 xdg.portal = {
   enable = true;

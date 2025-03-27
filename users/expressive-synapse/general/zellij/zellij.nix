4580d923d00d26b5fce1f3545db2060976c0ab14
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-27 00:24:00 -0400

    added plugins and reconfiguring zellij

--- users/expressive-synapse/general/zellij/zellij.nix
+++ users/expressive-synapse/general/zellij/zellij.nix
@@ -3,11 +3,26 @@
 {
 programs.zellij.enable = true;
 
-home.file.".config/zellij/config.kdl" = {
-  source = ./config.kdl;
-};
+  #home.file.".config/zellij/config.kdl" = {
+  #source = ./config.kdl;
+  #};
 home.file.".config/zellij/plugins" = {
   source = ./plugins;
+
+
 };
+
+ programs.zellij.settings = {
+  plugins = {
+      autolock = {
+        location = "file:~/.config/zellij/plugins/zellij-autolock.wasm";
+        is_enabled = true;
+        triggers = "vi|vim|nvim|ncats|yazi";
+        reaction_seconds = "0.3";
+        print_to_log = true;
+      };
+    };  
+  keybinds = {};  
+ };
 
 }

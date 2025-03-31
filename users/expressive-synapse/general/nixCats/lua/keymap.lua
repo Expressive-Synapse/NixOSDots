d61528f6365a8e9f81f1bf66aa503c10f752cec1
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-03-30 23:03:48 -0400

    moved plugins out of init.lua

--- users/expressive-synapse/general/nixCats/lua/keymap.lua
+++ users/expressive-synapse/general/nixCats/lua/keymap.lua
@@ -6,6 +6,13 @@ return {
 	-- Set highlight on search, but clear on pressing <Esc> in normal mode
 	vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>"),
 
+	--require("zellij-nav").setup()
+	--
+	--vim.keymap.set("n", "<M-h>", "<cmd>ZellijNavigateLeftTab<cr>", { desc = "Move focus to the left window" })
+	--vim.keymap.set("n", "<M-l>", "<cmd>ZellijNavigateRightTab<cr>", { desc = "Move focus to the right window" })
+	--vim.keymap.set("n", "<M-j>", "<cmd>ZellijNavigateDown<cr>", { desc = "Move focus to the lower window" })
+	--vim.keymap.set("n", "<M-k>", "<cmd>ZellijNavigateUp<cr>", { desc = "Move focus to the upper window" })
+
 	-- Diagnostic keymaps
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" }),
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" }),

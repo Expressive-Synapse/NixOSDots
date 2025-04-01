41febcd51175f72b6584c0cf411a84c497a19b9c
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-04-01 00:47:34 -0400

    removed depricated files

--- scripts/ags_v1/modules/brightness.js
+++
@@ -1,3 +0,0 @@
-const changeBrightness = (monitorId, value) => {
-  Utils.execAsync(`ddcutil -d ${monitorId} setvcp 10 ${value * 100}`)
-}
--- scripts/ags_v1/modules/clock.js
+++
@@ -1,7 +0,0 @@
-const date = Variable('',{
-  poll: [1000, `date +'%l:%M %p'`],
-});
-
-export const clock = () => Widget.Label({ label: date.bind(),
-  className: 'clock'})
-

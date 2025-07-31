b58cafd90b6217ccf49e5948b145a863d5fdbf6d
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-07-31 01:07:34 -0400

    set yazi and ghostty as xdg defaults

--- users/expressive-synapse/Titanic/terminal/ghostty.nix
+++ users/expressive-synapse/Titanic/terminal/ghostty.nix
@@ -1,4 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.ghostty.enable = true;
+  
+  xdg.desktopEntries.ghostty = {
+    name = "Ghostty";
+    exec = "${pkgs.ghostty}/bin/ghostty";
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/terminal" = "ghostty.desktop";
+    };
+  };
 }

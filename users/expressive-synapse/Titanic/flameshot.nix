99ce54875d3ef05433ee5da398f0d9ae911fee92
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-09-11 13:47:20 -0400

    fixed nesting issue

--- users/expressive-synapse/Titanic/flameshot.nix
+++ users/expressive-synapse/Titanic/flameshot.nix
@@ -1,23 +1,23 @@
 { pkgs, config, ... }:
 {
-services.flameshot = {
-  enable = true;
-  package = (pkgs.flameshot.override {
+  services.flameshot = {
+    enable = true;
+    package = (pkgs.flameshot.override {
       enableWlrSupport = true;
     });
-  };
-  settings = {
-    General = {
-      useGrimAdapter = true;
+    settings = {
+      General = {
+        useGrimAdapter = true;
 
-      disabledTrayIcon = true;
+        disabledTrayIcon = true;
 
-      savePath = "${config.home.homeDirectory}/Pictures/Screenshots";
-      savePathFixed = true; 
-      saveAsFileExtension = ".jpg";
-      filenamePattern = "%F_%H-%M";
-      drawThickness = 1;
-      copyPathAfterSave = true;
+        savePath = "${config.home.homeDirectory}/Pictures/Screenshots";
+        savePathFixed = true; 
+        saveAsFileExtension = ".jpg";
+        filenamePattern = "%F_%H-%M";
+        drawThickness = 1;
+        copyPathAfterSave = true;
+      };
     };
   };
-}
+  }

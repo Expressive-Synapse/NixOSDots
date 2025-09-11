51176c1e378abb7e6d92ddaeb356242a793cca7f
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-09-11 13:39:47 -0400

    added settings to flameshot

--- users/expressive-synapse/Titanic/flameshot.nix
+++ users/expressive-synapse/Titanic/flameshot.nix
@@ -1,9 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
 services.flameshot = {
   enable = true;
   package = (pkgs.flameshot.override {
       enableWlrSupport = true;
     });
+  };
+  settings = {
+    General = {
+      useGrimAdapter = true;
+
+      disabledTrayIcon = true;
+
+      savePath = "${config.home.homeDirectory}/Pictures/Screenshots";
+      savePathFixed = true; 
+      saveAsFileExtension = ".jpg";
+      filenamePattern = "%F_%H-%M";
+      drawThickness = 1;
+      copyPathAfterSave = true;
+    };
   };
 }

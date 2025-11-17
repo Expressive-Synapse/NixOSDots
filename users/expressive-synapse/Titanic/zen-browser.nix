7f7256866da2e6e89330bf4b6e0cf0aac69cab01
Expressive-Synapse <ConnorDGoff@protonmail.com>
2025-11-16 22:02:00 -0500

    set zen-browser as default browser

--- users/expressive-synapse/Titanic/zen-browser.nix
+++ users/expressive-synapse/Titanic/zen-browser.nix
@@ -1,4 +1,4 @@
-{inputs, ... }:
+{inputs, config, pkgs, ... }:
 {
   imports = [
     inputs.zen-browser.homeModules.beta
@@ -17,4 +17,33 @@
       "expressive-synapse" = {};
     };
   };
+
+  xdg.mimeApps = let
+      associations = builtins.listToAttrs (map (name: {
+          inherit name;
+          value = let
+            zen-browser = config.programs.zen-browser.package;
+          in
+            zen-browser.meta.desktopFileName;
+        }) [
+          "application/x-extension-shtml"
+          "application/x-extension-xhtml"
+          "application/x-extension-html"
+          "application/x-extension-xht"
+          "application/x-extension-htm"
+          "x-scheme-handler/unknown"
+          "x-scheme-handler/mailto"
+          "x-scheme-handler/chrome"
+          "x-scheme-handler/about"
+          "x-scheme-handler/https"
+          "x-scheme-handler/http"
+          "application/xhtml+xml"
+          "application/json"
+          "text/plain"
+          "text/html"
+        ]);
+     in {
+      associations.added = associations;
+      defaultApplications = associations;
+    };
 }

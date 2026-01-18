1984404dab6dc3ad15bb6948d1287e99a7f30cfc
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-01-17 23:09:19 -0500

    removed manual import for home-manager

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       url = "nixpkgs/nixos-23.11";
     };
 
-    home-manager = {
+    /*home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -20,7 +20,7 @@
     home-manager-stable = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
-    };
+    };*/
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -99,7 +99,7 @@
       "ServerBoy" = lib.nixosSystem {
 	      system = "x86_64-linux";
 	      modules = [
-          inputs.home-manager.nixosModules.home-manager
+          #inputs.home-manager.nixosModules.home-manager
           inputs.disko.nixosModules.default
           inputs.impermanence.nixosModules.impermanence
           inputs.sops-nix.nixosModules.sops

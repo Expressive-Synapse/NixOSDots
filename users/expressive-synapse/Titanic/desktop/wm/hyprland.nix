9aecc6c156cea65574b85e22c1dfa21501630f67
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-01-18 15:34:52 -0500

    added hyprsplit plugin

--- users/expressive-synapse/Titanic/desktop/wm/hyprland.nix
+++ users/expressive-synapse/Titanic/desktop/wm/hyprland.nix
@@ -23,6 +23,7 @@ wayland.windowManager.hyprland = {
 
 wayland.windowManager.hyprland.plugins = [
   #inputs.split-monitor-workspaces.packages.${pkgs.stdenv.hostPlatform.system}.split-monitor-workspaces
+  inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprsplit
 ];
 
 wayland.windowManager.hyprland.settings = {

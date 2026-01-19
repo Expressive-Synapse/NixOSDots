2142fc60d592008b654df2b7334203ec9219ba87
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-01-18 19:01:09 -0500

    fixed missing ;

--- hosts/Titanic/deviceInterfacing.nix
+++ hosts/Titanic/deviceInterfacing.nix
@@ -1,37 +1,37 @@
 { pkgs, ... }:
 
 {
-environment.systemPackages = with pkgs; [
-  #mtpfs
-  simple-mtpfs
-  piper
-  usbutils
-  jmtpfs
-  glib
-  via
-  cdemu-daemon
-  antimicrox
-  linuxConsoleTools
-];
+  environment.systemPackages = with pkgs; [
+    #mtpfs
+    simple-mtpfs
+    piper
+    usbutils
+    jmtpfs
+    glib
+    via
+    cdemu-daemon
+    antimicrox
+    linuxConsoleTools
+  ];
 
-programs.kdeconnect.enable = true;
+  programs.kdeconnect.enable = true;
 
-programs.cdemu.enable = true;
+  programs.cdemu.enable = true;
 
-services.ratbagd.enable = true;
+  services.ratbagd.enable = true;
 
-services.gvfs.enable = true;
+  services.gvfs.enable = true;
 
-services.hardware.openrgb.enable = true;
+  services.hardware.openrgb.enable = true;
 
-hardware.keyboard.qmk.enable = true;
+  hardware.keyboard.qmk.enable = true;
 
-services.udev.packages = with pkgs; [
-  via
-];
-services.udev.extraRules = ''
-#8bitdo
-SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="5200", MODE="0666"
-SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="5201", MODE="0666"
-  ''
+  services.udev.packages = with pkgs; [
+    via
+  ];
+  services.udev.extraRules = ''
+    #8bitdo
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="5200", MODE="0666"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2dc8", ATTRS{idProduct}=="5201", MODE="0666"
+  '';
 }

b4aaa397581390a89ca67264bb335dfba51e708e
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-03-07 13:05:44 -0500

    added parsec

--- hosts/Titanic/gaming.nix
+++ hosts/Titanic/gaming.nix
@@ -1,41 +1,45 @@
 { pkgs, ... }:
 
 {
-nixpkgs.config.packageOverrides = pkgs: {
+  nixpkgs.config.packageOverrides = pkgs: {
     steam = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
-      ];
+      extraPkgs =
+        pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
     };
   };
 
- environment.systemPackages = with pkgs; [
-    (retroarch.withCores (cores: with cores; [
-      genesis-plus-gx
-      snes9x
-      beetle-psx-hw
-      mame
-      beetle-pcfx
-      beetle-pce
-      mupen64plus
-  ]))
-  mangohud
-  protontricks
- ];
+  environment.systemPackages = with pkgs; [
+    (retroarch.withCores (
+      cores: with cores; [
+        genesis-plus-gx
+        snes9x
+        beetle-psx-hw
+        mame
+        beetle-pcfx
+        beetle-pce
+        mupen64plus
+      ]
+    ))
+    mangohud
+    protontricks
+    parsec-bin
+  ];
 
- programs.steam.enable = true;
- programs.steam.gamescopeSession.enable = true;
- programs.gamemode.enable = true;
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
 
- services.xserver.desktopManager.retroarch.enable = true;
- 
+  services.xserver.desktopManager.retroarch.enable = true;
+
 }

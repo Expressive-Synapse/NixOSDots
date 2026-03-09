1f48d7e3587ecdea2961a37efff79c4cd50482fb
Expressive-Synapse <ConnorDGoff@protonmail.com>
2026-03-09 14:44:58 -0400

    changed xdg mime code

--- users/expressive-synapse/Titanic/zen-browser.nix
+++ users/expressive-synapse/Titanic/zen-browser.nix
@@ -1,4 +1,4 @@
-{inputs, config, pkgs, ... }:
+{ inputs, system, ... }:
 {
   imports = [
     inputs.zen-browser.homeModules.beta
@@ -14,35 +14,42 @@
   programs.zen-browser = {
     enable = true;
     profiles = {
-      "expressive-synapse" = {};
+      "expressive-synapse" = { };
     };
   };
+  xdg.mimeApps =
+    let
+      value =
+        let
+          zen-browser = inputs.zen-browser.packages.${system}.beta; # or twilight
+        in
+        zen-browser.meta.desktopFileName;
 
-  xdg.mimeApps = let
-      associations = builtins.listToAttrs (map (name: {
-          inherit name;
-          value = let
-            zen-browser = config.programs.zen-browser.package;
-          in
-            zen-browser.meta.desktopFileName;
-        }) [
-          "application/x-extension-shtml"
-          "application/x-extension-xhtml"
-          "application/x-extension-html"
-          "application/x-extension-xht"
-          "application/x-extension-htm"
-          "x-scheme-handler/unknown"
-          "x-scheme-handler/mailto"
-          "x-scheme-handler/chrome"
-          "x-scheme-handler/about"
-          "x-scheme-handler/https"
-          "x-scheme-handler/http"
-          "application/xhtml+xml"
-          "application/json"
-          "text/plain"
-          "text/html"
-        ]);
-     in {
+      associations = builtins.listToAttrs (
+        map
+          (name: {
+            inherit name value;
+          })
+          [
+            "application/x-extension-shtml"
+            "application/x-extension-xhtml"
+            "application/x-extension-html"
+            "application/x-extension-xht"
+            "application/x-extension-htm"
+            "x-scheme-handler/unknown"
+            "x-scheme-handler/mailto"
+            "x-scheme-handler/chrome"
+            "x-scheme-handler/about"
+            "x-scheme-handler/https"
+            "x-scheme-handler/http"
+            "application/xhtml+xml"
+            "application/json"
+            "text/plain"
+            "text/html"
+          ]
+      );
+    in
+    {
       associations.added = associations;
       defaultApplications = associations;
     };
